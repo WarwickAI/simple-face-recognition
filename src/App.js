aea8b34e25fc6a60e94312543aec74170a134575
Joe Hewett <joehewett1@gmail.com>
2021-09-27 00:18:23 +0100

    Working version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-has-content */
+// Import some useful react things
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// Import some CSS to make everything look a bit nicer
 import './App.css';
 
+// Import Amplify and Predictions so that we can configure a new predictions provider
+import Amplify, { Predictions } from 'aws-amplify';
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
+import awsconfig from './aws-exports';
+
+// Set up the predictions plugin using our default config
+Amplify.configure(awsconfig);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
+
 function App() {
+  // Declare some variables to save some values that we'll need later
+  const [name, setName] = useState("")
+  const [imageURL, setImageURL] = useState("");
+  const [links, setLinks] = useState([]);
+
+  // A function that takes an image and tells us who is in it
+  function identifyFromFile(event) {
+    // While the result is loading, say "Searching..."
+    setName('Searching...');
+    
+    // Get the file that the user uploaded
+    const file = event.target.files[0];
+
+    // Create a URL that we can use to display the photo
+    setImageURL(URL.createObjectURL(file));
+
+    // If we can't find a file, cancel. 
+    if (!file) {
+      return;
+    }
+
+    // Do the magic! 
+    Predictions.identify({
+      entities: {
+        source: {
+          file,
+        },
+        celebrityDetection: true
+      }
+    }).then(result => {
+      // From the result, get first entity detected. Get the metadata of that entity
+      const celebrityData = result.entities[0].metadata;
+      // Set the name and any links we find
+      setName(celebrityData.name);
+
+      if (celebrityData.urls) {
+        setLinks(celebrityData.urls)
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>Whose face is that?</h3>
+        <input type="file" onChange={identifyFromFile}></input>
+        <p>Upload an image</p>
+        { imageURL && <img className="uploadedImage" src={imageURL} alt="uploaded" ></img>}
+        <h3>{name}</h3>
+        <ul>
+        { links.map((link) => <p>{link}</p>)}
+        </ul>
+      </div>
     </div>
   );
 }
